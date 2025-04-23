582b99d3f458594f6b67924c8059e1fe76ed06c4
vasant-cloudside <vasanth.s@thecloudside.com>
2025-04-23 07:51:50 +0000

    Your commit message

---
+++ inter_annual_enterprise_survey.sql
@@ -0,0 +1,19 @@
+With
+{{config(materialized='view')}}
+
+inter_survey as (
+SELECT
+     {{ dbt_utils.star(from=ref('stg_annual_enterprise_survey01'),except=["ID1","Year"]) }},
+     {{ dbt_utils.star(from=ref('stg_annual_enterprise_survey02')) }}
+FROM
+     {{ ref('stg_annual_enterprise_survey02') }} AS t2
+LEFT JOIN
+     {{ ref('stg_annual_enterprise_survey01') }} AS t1
+ON
+    t1.ID1=t2.ID2
+WHERE
+     t2.Value <> 'C' AND t1.value1 <> 'C'
+
+)
+
+select * from inter_survey
